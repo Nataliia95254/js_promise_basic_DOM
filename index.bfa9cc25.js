1db85a2fafe6a7b062d56a5fd87253bcbecb07a3
Nataliia <koreniuknataliia@gmail.com>
2024-02-08 14:23:49 +0200

    make build

---
+++ index.bfa9cc25.js
@@ -0,0 +1,3 @@
+var e=function(e,n){var t=document.createElement("div");t.className=e,t.textContent=n,document.body.appendChild(t)},n=new Promise(function(e){document.querySelector(".logo").addEventListener("click",function(){e("Promise was resolved!")})}),t=new Promise(function(e,n){setTimeout(function(){return n(Error("Promise was rejected!"))},3e3)});n.then(function(n){e("message",n)}),t.catch(function(n){e("message error-message",n)});//# sourceMappingURL=index.bfa9cc25.js.map
+
+//# sourceMappingURL=index.bfa9cc25.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const createDivElement = (typeMessage, result)=>{
+    const messageDiv = document.createElement("div");
+    messageDiv.className = typeMessage;
+    messageDiv.textContent = result;
+    document.body.appendChild(messageDiv);
+};
+const promise1 = new Promise((resolve)=>{
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=>{
+        resolve("Promise was resolved!");
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>reject(new Error("Promise was rejected!")), 3000);
+});
+promise1.then((message)=>{
+    createDivElement("message", message);
+});
+promise2.catch((error)=>{
+    createDivElement("message error-message", error);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
